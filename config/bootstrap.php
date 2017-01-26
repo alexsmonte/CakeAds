417d5ec8810201c8c146007a62e59137183eb84d
Alex Monte <alexsmonte@gmail.com>
2017-01-26 15:07:30 -0400

    Implementing basic settings.

---
+++ config/bootstrap.php
@@ -0,0 +1,14 @@
+<?php
+use Cake\Core\Configure;
+
+if (!Configure::read('CakeAds.type')) {
+    Configure::write('CakeAds.type', ['I' => __('Image'), 'T' => __('Text')]);
+}
+
+if (!Configure::read('CakeAds.path')) {
+    Configure::write('CakeAds.path', 'ads{DS}{year}{DS}{month}{DS}');
+}
+
+if (!Configure::read('CakeAds.keepFilesOnDelete')) {
+    Configure::write('CakeAds.keepFilesOnDelete', false);
+}
